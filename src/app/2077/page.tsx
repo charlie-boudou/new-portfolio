978cd462e78ac9da6bc4162fe6ce060c0ca8cd15
charlieboudou <charlie.lcb3@gmail.com>
2026-02-04 01:24:01 +0400

    background 2077

--- src/app/2077/page.tsx
+++ src/app/2077/page.tsx
@@ -1,14 +1,45 @@
-import FutureProjectsList from "../../components/2077/FutureProjectsList";
+import FutureProjectsList from "@/components/2077/FutureProjectsList";
+import Image from "next/image";
 
 export default function FutureHome() {
   return (
-    <div className="w-full h-screen p-[1rem] future-font">
-      <div className="w-full h-full border-2 border-[#22d3ee] flex items-center justify-center">
-        {/*<div className="border-2 border-[#22d3ee] w-[20%] h-[30%]">about me</div>*/}
-        <div className="relative w-[28%] h-[40%] flex items-center justify-center p-[2px] rounded-full bg-gradient-to-b from-cyan-400 to-purple-600">
-          <div className="bg-[#050505] w-full h-full rounded-full p-[1rem] text-white flex items-center justify-center">
-            <FutureProjectsList />
-          </div>
+    <div className="w-full h-screen p-[1rem] future-font bg-[url('/images/futureBackground.jpg')] bg-cover bg-center bg-no-repeat">
+      <div className="w-full h-full">
+        <svg 
+          className="absolute inset-0 w-full h-full pointer-events-none" 
+          viewBox="0 0 100 100" 
+          preserveAspectRatio="none"
+          style={{ overflow: 'visible' }}
+        >
+            <path 
+              d="M 13,3 L 87,3 L 97,13 L 97,87 L 87,97 L 13,97 L 3,87 L 3,13 Z"
+              fill="rgba(29, 41, 61, 0.9)"
+            />
+            <path 
+              d="M 13,3 L 87,3 L 97,13 L 97,87 L 87,97 L 13,97 L 3,87 L 3,13 Z"
+              fill="none" 
+              stroke="#22d3ee" 
+              strokeWidth="2" 
+              vectorEffect="non-scaling-stroke"
+            />
+            <line x1="30" y1="3" x2="70" y2="3" stroke="#22d3ee" strokeWidth="10" vectorEffect="non-scaling-stroke" />
+            <line x1="30" y1="97" x2="70" y2="97" stroke="#22d3ee" strokeWidth="10" vectorEffect="non-scaling-stroke" />
+        </svg>
+        <div className="relative w-full flex items-center justify-end">
+          {/*<div className="relative w-[30%] top-[5rem] right-[5rem] flex items-center justify-center p-[2px] rounded-[1rem] bg-cyan-400">
+            <div className="bg-slate-800 w-full h-full rounded-[1rem] p-[1rem] text-white flex flex-col items-center justify-center space-y-[.5rem]">
+              <p>about me</p>
+              <Image src="/images/me.png" alt="picture" width={400} height={400} className="w-[6rem] h-[8rem]" />
+            </div>
+          </div>*/}
+        </div>
+        <div className="w-full h-full relative flex items-center justify-center">
+          UNDER CONSTRUCTION
+          {/*<div className="relative w-[90%] md:w-[28%] h-[38%] md:h-[48%] flex items-center justify-center p-[2px] rounded-full bg-gradient-to-b from-cyan-400 to-[#CF5CCD] shadow-[0_0_20px_rgba(34,211,238,0.5)]">
+            <div className="bg-slate-800 w-full h-full rounded-full p-[1rem] text-white flex items-center justify-center">
+              <FutureProjectsList /> 
+            </div>
+          </div>*/}
         </div>
       </div>
     </div>
--- src/components/2077/FutureProjectsList.tsx
+++ src/components/2077/FutureProjectsList.tsx
@@ -1,34 +1,28 @@
 'use client';
 export default function FutureProjectList() {
-  const projects = ["OPX", "Villa Calm", "Tikok", "Drapo"];
+  const projects = ["OPX", "Villa Calm", "Tikok", "Drapo", "Ma plus belle toile", "Memories", "Le Comptoir des Arcs"];
 
   return (
     <div className="flex flex-col md:flex-row items-center justify-between gap-12 p-4">
-
-      {/* Section Droite : Les Nœuds (Hexagones) */}
-      <div className="relative w-64 h-64 flex items-center justify-center">
-        {/* Cercle central Glow */}
+      < div className="relative w-64 h-64 flex items-center justify-center">
         <div className="absolute w-32 h-32 bg-cyan-500/20 rounded-full blur-3xl animate-pulse" />
-        
-        {/* Logo Central */}
-        <div className="z-20 p-[2px] rounded-xl bg-gradient-to-br from-cyan-400 to-purple-600 shadow-[0_0_20px_rgba(34,211,238,0.5)]">
-           <div className="bg-slate-900 p-4 rounded-[10px] flex items-center justify-center">
-              <span className="text-3xl font-bold italic">N</span>
-           </div>
-        </div>
-
-        {/* Projets satellites */}
-        {projects.map((name, i) => (
-          <div 
-            key={name}
-            className="absolute p-2 border border-cyan-500/30 bg-slate-900/80 rounded-lg text-[10px] hover:border-cyan-400 transition-colors cursor-pointer shadow-lg shadow-cyan-500/10"
-            style={{
-              transform: `rotate(${i * 90}deg) translate(100px) rotate(-${i * 90}deg)`
-            }}
-          >
-            {name}
-          </div>
-        ))}
+            <div className="z-20 p-[2px] rounded-xl bg-cyan-400 shadow-[0_0_20px_rgba(34,211,238,0.5)]">
+                <div className="bg-slate-800 p-4 rounded-[10px] flex items-center justify-center">
+                    <span className="text-3xl font-bold">{`${"</>"}`}</span>
+                </div>
+            </div>
+            <div className="absolute border-2 border-dotted border-[#CF5CCD] w-full h-[98%] rounded-full" />
+            {projects.map((name, i) => (
+                <div 
+                    key={name}
+                    className="absolute bg-slate-800 truncate text-center w-[5rem] p-2 border border-cyan-400 rounded-lg text-[10px] hover:border-cyan-400 transition-colors cursor-pointer shadow-lg shadow-cyan-500/10"
+                    style={{
+                    transform: `rotate(${i * 60}deg) translate(120px) rotate(-${i * 60}deg)`
+                    }}
+                >
+                    {name}
+                </div>
+            ))}
       </div>
     </div>
   );
